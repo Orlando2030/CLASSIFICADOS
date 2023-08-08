eea596f76cd1008e7092c742993abea0692397c4
Orlando Santos de Olveira <orlando2030@gmail.com>
2023-08-08 17:02:13 -0300

    Indentação classe MeuDbContext

--- src/DATA/Context/MeuDbContext.cs
+++ src/DATA/Context/MeuDbContext.cs
@@ -14,19 +14,17 @@ namespace DATA.Context
         {
             
         }
-
-        public DbSet<Anuncio> Anuncios { get; set; }
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<SubCategoria> SubCategorias { get; set; }
-        public DbSet<Endereco> Enderecos { get; set; }
-        public DbSet<Loja> Lojas { get; set; }
-        public DbSet<Marca> Marcas { get; set; }
-        public DbSet<Mensagem> Mensagens { get; set; }
-        public DbSet<Modelo> Modelos { get; set; }
-        public DbSet<Pedido> Pedidos { get; set; }
-        public DbSet<TipoAnuncio> TiposAnuncios { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
-
+            public DbSet<Anuncio> Anuncios { get; set; }
+            public DbSet<Categoria> Categorias { get; set; }
+            public DbSet<SubCategoria> SubCategorias { get; set; }
+            public DbSet<Endereco> Enderecos { get; set; }
+            public DbSet<Loja> Lojas { get; set; }
+            public DbSet<Marca> Marcas { get; set; }
+            public DbSet<Mensagem> Mensagens { get; set; }
+            public DbSet<Modelo> Modelos { get; set; }
+            public DbSet<Pedido> Pedidos { get; set; }
+            public DbSet<TipoAnuncio> TiposAnuncios { get; set; }
+            public DbSet<Usuario> Usuarios { get; set; }
     }
 
 }
